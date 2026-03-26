224d320dd3248ac5c0725eb5333178050093ba8c
Frederico D. Sasek <fsasek6@gmail.com>
2026-03-26 09:56:27 -0300

    aham

--- main.cpp
+++ main.cpp
@@ -4,23 +4,6 @@
 using namespace std;
 
 int main(){
-    ListaCont m(4);
-    m.insere(1);
-    m.insere(2);
-    m.insere(4);
-    m.insere(5);
-    //m.imprime();
-    m.insereK(3,2);
-    m.insereInicio(0);
-    m.insere(6);
-    m.insereInicio(7);
-    //m.imprime();
-    //cout << m.existe(3);
-    m.ordena();
-    //m.imprime();
-    //m.limpa();
-    //cout << m.menor();
-    //m.imprime();
-    m.help();
+
     return 0;
 }
